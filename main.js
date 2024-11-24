32e369a79d265b3213c8346000eac7e697c9d1b5
Sahil-Coder1 <26maysahil@gmail.com>
2024-11-24 16:27:01 +0530

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementsByTagName("form")[0].style.backgroundColor = "#3C3D37";
+const rows = document.querySelectorAll(".m-movie-body");
+rows.forEach((row, index) => {
+    row.style.backgroundColor = "#444444";
+});
+const heads = document.querySelectorAll(".m-movie-title");
+heads.forEach((head, index) => {
+    head.style.cssText = `
+    background: linear-gradient(to right, #002945,#2196f3 );`;
+});  
---
+++ timetable.js
@@ -0,0 +1,21 @@
+const rows = document.querySelectorAll("table th, table td");
+rows.forEach((row, index) => {
+  row.style.color = "#FAF7F0";
+  row.style.border = "0px solid #fff";
+
+  if (index % 2 === 0) {
+    row.style.backgroundColor = "#3C3D37"; // Darker for even rows
+  } else {
+    row.style.backgroundColor = "#444444"; // Lighter for odd rows
+  }
+});
+
+document.getElementById("form1").style.backgroundColor = "#3C3D37";
+document.getElementsByClassName("m-movie-title")[0].style.cssText = `
+background: linear-gradient(to right, #2196f3,#444444 );
+`;
+document.getElementsByClassName("m-movie-body")[0].style.cssText = `
+background-color:#444444;
+border:0px solid #fff;
+color:red;
+`;
