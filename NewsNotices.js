71613bfa81a0542757f7ef6b34b95b1f401b2d31
Sahil-Coder1 <26maysahil@gmail.com>
2024-12-22 21:41:11 +0530

    updated news section

--- NewsNotices.js
+++ NewsNotices.js
@@ -11,3 +11,12 @@ const headings = document.querySelectorAll("h2");
 headings.forEach((row, index) => {
   row.style.backgroundColor = "#383838";
 });
+
+const head = document.querySelectorAll("resp-accordion hor_1");
+
+setTimeout(() => {
+  head.forEach((row) => {
+    // row.style.cssText = ""; 
+    row.style.cssText = "background-color:rgb(226, 22, 22);"; 
+  });
+}, 2000); // 1000 milliseconds = 1 second
