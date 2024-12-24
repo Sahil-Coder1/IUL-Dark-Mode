7d10a7bfe8ac94c33931e30671f82554dbc5d3ae
Sahil-Coder1 <26maysahil@gmail.com>
2024-12-24 21:54:59 +0530

    date wise attendance

---
+++ Datewiseattendance.js
@@ -0,0 +1,19 @@
+document.getElementsByTagName("form")[0].style.backgroundColor = "#1E1E1E";
+
+const rows = document.querySelectorAll(".m-movie-body");
+rows.forEach((row, index) => {
+  row.style.backgroundColor = index % 2 === 0 ? "#505050" : "#383838";
+});
+
+const heads = document.querySelectorAll(".m-movie-title");
+heads.forEach((head) => {
+  head.style.cssText = `background: linear-gradient(to right, #1A237E, #64B5F6); color: white; padding: 5px; border-radius: 5px;`;
+});
+
+
+
+const listItems = document.querySelectorAll(".m-movie-body a");
+listItems.forEach((heading) => {
+  heading.style.color = "#B0B3B8";
+});
+
